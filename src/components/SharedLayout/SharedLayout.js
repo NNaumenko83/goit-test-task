3a71f56f965a6f4066983ced54161832799c5692
Mykola <108565397+NNaumenko83@users.noreply.github.com>
2023-06-21 00:03:31 +0300

    Make footer to bottom

--- src/components/SharedLayout/SharedLayout.js
+++ src/components/SharedLayout/SharedLayout.js
@@ -1,23 +1,42 @@
 // Imports
 import { Outlet } from "react-router-dom";
-import { Container, Header, Link, Logo } from "./SharedLayout.styled";
+import {
+  Container,
+  HeaderContainer,
+  Footer,
+  Header,
+  Link,
+  Logo,
+  Main,
+} from "./SharedLayout.styled";
 import { Suspense } from "react";
 
 const SharedLayout = () => {
   return (
-    <Container>
+    <>
       <Header>
-        <Logo>GoIt Test Task</Logo>
-
-        <nav>
-          <Link to="/">Home</Link>
-          <Link to="/tweets">Tweets</Link>
-        </nav>
+        <HeaderContainer>
+          <Logo>GoIt Test Task</Logo>
+          <nav>
+            <Link to="/">Home</Link>
+            <Link to="/tweets">Tweets</Link>
+          </nav>
+        </HeaderContainer>
       </Header>
-      <Suspense fallback={<div>Loading...</div>}>
-        <Outlet />
-      </Suspense>
-    </Container>
+      <Main>
+        <Container>
+          <Suspense fallback={<div>Loading...</div>}>
+            <Outlet />
+          </Suspense>
+        </Container>
+      </Main>
+
+      <Footer>
+        <Container>
+          <p>Footer</p>
+        </Container>
+      </Footer>
+    </>
   );
 };
 
--- src/components/SharedLayout/SharedLayout.styled.js
+++ src/components/SharedLayout/SharedLayout.styled.js
@@ -1,10 +1,51 @@
-import styled from 'styled-components';
-import { NavLink } from 'react-router-dom';
+import styled from "styled-components";
+import { NavLink } from "react-router-dom";
 
 export const Container = styled.div`
-  max-width: 960px;
+  display: flex;
   margin: 0 auto;
-  padding: 0 16px;
+  min-width: 320px;
+  height: 100%;
+  align-items: center;
+  padding: 15px 30px;
+  background-color: ${(props) => props.theme.header};
+
+  outline: 1px solid red;
+
+  @media screen and (min-width: 480px) {
+    width: 480px;
+  }
+
+  @media screen and (min-width: 768px) {
+    width: 768px;
+  }
+  @media screen and (min-width: 1200px) {
+    width: 1200px;
+  }
+`;
+
+export const HeaderContainer = styled.div`
+  display: flex;
+  justify-content: space-between;
+  margin: 0 auto;
+  min-width: 320px;
+  height: 100%;
+  align-items: center;
+  padding: 15px 30px;
+  background-color: ${(props) => props.theme.header};
+
+  outline: 1px solid red;
+
+  @media screen and (min-width: 480px) {
+    width: 480px;
+  }
+
+  @media screen and (min-width: 768px) {
+    width: 768px;
+  }
+  @media screen and (min-width: 1200px) {
+    width: 1200px;
+  }
 `;
 
 export const Header = styled.header`
@@ -37,4 +78,14 @@ export const Link = styled(NavLink)`
     color: white;
     background-color: #0181e4;
   }
+`;
+
+export const Footer = styled.footer`
+  margin-top: auto;
+  /* outline: 1px solid purple; */
+  background-color: pink;
+`;
+
+export const Main = styled.main`
+  flex-grow: 1;
 `;
