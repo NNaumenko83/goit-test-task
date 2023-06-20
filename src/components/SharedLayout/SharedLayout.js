6ef8515d4e915881a803fd66fe752d13f6f06e24
Mykola <108565397+NNaumenko83@users.noreply.github.com>
2023-06-20 22:03:50 +0300

    test

--- src/components/SharedLayout/SharedLayout.js
+++ src/components/SharedLayout/SharedLayout.js
@@ -1,13 +1,14 @@
 // Imports
-import { Outlet } from 'react-router-dom';
-import { Container, Header, Link, Logo } from './SharedLayout.styled';
-import { Suspense } from 'react';
+import { Outlet } from "react-router-dom";
+import { Container, Header, Link, Logo } from "./SharedLayout.styled";
+import { Suspense } from "react";
 
 const SharedLayout = () => {
   return (
     <Container>
       <Header>
         <Logo>GoIt Test Task</Logo>
+        <p>Test</p>
         <nav>
           <Link to="/">Home</Link>
           <Link to="/template">Template</Link>
